82b54b79da4476ae7489eaf2d5299897503eb7ac
Anshuman <anshumankumar.899@gmail.com>
2026-05-27 21:18:03 +0530

    Point frontend to MongoDB backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 // Deployed backend:
-window.TODO_API_URL = "https://t0-do-i7v6.onrender.com";
+window.TODO_API_URL = "https://t0-do-1.onrender.com";
 
 // Local development backend:
 // window.TODO_API_URL = "http://localhost:4000";
